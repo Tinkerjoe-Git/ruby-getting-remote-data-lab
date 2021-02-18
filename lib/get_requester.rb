dabeb2c62325a3ff76bcdf7398341d25de20e379
Tinkergit <nicholas.stagno@gmail.com>
2021-02-18 14:12:16 -0500

    commit tree 8abd9faaf2358d40d4f6ca255a0656ecf3f38714

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,34 @@
-# Write your code here
+require 'json'
+require 'open-uri'
+require 'net/http'
+
+
+class GetRequester
+
+    attr_accessor :url
+
+    def initialize(url)
+        @url=url
+    end
+
+    def get_response_body
+        uri = URI.parse(URL)
+        response = Net::HTTP.get_response(uri)
+        response.body
+    end
+
+
+    def parse_json
+        request = JSON.parse(get_response_body)
+    end
+
+        
+end
+
+
+
+
+
+#GetRequester.new("https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json")
+
+
